4001014f867a5b96923a68bd925280d9d9107df7
RyanSquared <vandor2012@gmail.com>
2018-07-03 17:00:00 -0500

    Add initial program and remove bootstrap script

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-# Check for Python packages
-
-python3 --version || sudo apt install -y python3
-pip3 --version || sudo apt install -y python3-pip
-
-# Install the AWS IoT Python tooling
-
-pip3 install --user --upgrade AWSIoTPythonSDK
---
+++ setup.py
@@ -0,0 +1,8 @@
+# pylint: disable=missing-docstring
+from distutils.core import setup
+
+setup(
+    name='noticast-iot-core',
+    version='0.1-dev',
+    packages=['noticast'],
+    install_requires=['AWSIoTPythonSDK', 'requests'])
